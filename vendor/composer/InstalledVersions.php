4044b2dae5159f46df0e3b8e509ba9a59ddc4f0a
luz <luz@macbook-air-de-luz.home>
2020-10-25 14:59:48 +0100

    7

---
+++ vendor/composer/InstalledVersions.php
@@ -0,0 +1,453 @@
+<?php
+
+namespace Composer;
+
+use Composer\Semver\VersionParser;
+
+
+
+
+
+
+class InstalledVersions
+{
+private static $installed = array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '19d751ed4b3d2adb4f73aab442de2f943694f04b',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '19d751ed4b3d2adb4f73aab442de2f943694f04b',
+    ),
+    'clue/stream-filter' => 
+    array (
+      'pretty_version' => 'v1.5.0',
+      'version' => '1.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'aeb7d8ea49c7963d3b581378955dbf5bc49aa320',
+    ),
+    'guzzlehttp/guzzle' => 
+    array (
+      'pretty_version' => '6.5.5',
+      'version' => '6.5.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9d4290de1cfd701f38099ef7e183b64b4b7b0c5e',
+    ),
+    'guzzlehttp/promises' => 
+    array (
+      'pretty_version' => '1.4.0',
+      'version' => '1.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '60d379c243457e073cff02bc323a2a86cb355631',
+    ),
+    'guzzlehttp/psr7' => 
+    array (
+      'pretty_version' => '1.7.0',
+      'version' => '1.7.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '53330f47520498c0ae1f61f7e2c90f55690c06a3',
+    ),
+    'knplabs/github-api' => 
+    array (
+      'pretty_version' => 'v2.15.0',
+      'version' => '2.15.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '03445f26843ec44319fe1f3c2a8ef6fcd545a154',
+    ),
+    'paragonie/random_compat' => 
+    array (
+      'pretty_version' => 'v9.99.100',
+      'version' => '9.99.100.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '996434e5492cb4c3edcb9168db6fbb1359ef965a',
+    ),
+    'php-http/async-client-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'php-http/cache-plugin' => 
+    array (
+      'pretty_version' => '1.7.1',
+      'version' => '1.7.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a1cb3e2a4f095f820e0a0e078f3dfd2d9c83a0f7',
+    ),
+    'php-http/client-common' => 
+    array (
+      'pretty_version' => '1.10.0',
+      'version' => '1.10.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c0390ae3c8f2ae9d50901feef0127fb9e396f6b4',
+    ),
+    'php-http/client-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'php-http/discovery' => 
+    array (
+      'pretty_version' => '1.12.0',
+      'version' => '1.12.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4366bf1bc39b663aa87459bd725501d2f1988b6c',
+    ),
+    'php-http/guzzle6-adapter' => 
+    array (
+      'pretty_version' => 'v1.1.1',
+      'version' => '1.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a56941f9dc6110409cfcddc91546ee97039277ab',
+    ),
+    'php-http/httplug' => 
+    array (
+      'pretty_version' => 'v1.1.0',
+      'version' => '1.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1c6381726c18579c4ca2ef1ec1498fdae8bdf018',
+    ),
+    'php-http/message' => 
+    array (
+      'pretty_version' => '1.9.1',
+      'version' => '1.9.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '09f3f13af3a1a4273ecbf8e6b27248c002a3db29',
+    ),
+    'php-http/message-factory' => 
+    array (
+      'pretty_version' => 'v1.0.2',
+      'version' => '1.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a478cb11f66a6ac48d8954216cfed9aa06a501a1',
+    ),
+    'php-http/message-factory-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'php-http/promise' => 
+    array (
+      'pretty_version' => '1.1.0',
+      'version' => '1.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4c4c1f9b7289a2ec57cde7f1e9762a5789506f88',
+    ),
+    'psr/cache' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd11b50ad223250cf17b86e38383413f5a6764bf8',
+    ),
+    'psr/http-message' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f6561bf28d520154e4b0ec72be95418abe6d9363',
+    ),
+    'psr/http-message-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'ralouphie/getallheaders' => 
+    array (
+      'pretty_version' => '3.0.3',
+      'version' => '3.0.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '120b605dfeb996808c31b6477290a714d356e822',
+    ),
+    'symfony/deprecation-contracts' => 
+    array (
+      'pretty_version' => 'v2.2.0',
+      'version' => '2.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5fa56b4074d1ae755beb55617ddafe6f5d78f665',
+    ),
+    'symfony/options-resolver' => 
+    array (
+      'pretty_version' => 'v5.1.7',
+      'version' => '5.1.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4c7e155bf7d93ea4ba3824d5a14476694a5278dd',
+    ),
+    'symfony/polyfill-intl-idn' => 
+    array (
+      'pretty_version' => 'v1.19.0',
+      'version' => '1.19.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4ad5115c0f5d5172a9fe8147675ec6de266d8826',
+    ),
+    'symfony/polyfill-intl-normalizer' => 
+    array (
+      'pretty_version' => 'v1.19.0',
+      'version' => '1.19.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8db0ae7936b42feb370840cf24de1a144fb0ef27',
+    ),
+    'symfony/polyfill-php70' => 
+    array (
+      'pretty_version' => 'v1.19.0',
+      'version' => '1.19.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3fe414077251a81a1b15b1c709faf5c2fbae3d4e',
+    ),
+    'symfony/polyfill-php72' => 
+    array (
+      'pretty_version' => 'v1.19.0',
+      'version' => '1.19.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'beecef6b463b06954638f02378f52496cb84bacc',
+    ),
+    'symfony/polyfill-php80' => 
+    array (
+      'pretty_version' => 'v1.19.0',
+      'version' => '1.19.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f54ef00f4678f348f133097fa8c3701d197ff44d',
+    ),
+  ),
+);
+
+
+
+
+
+
+
+public static function getInstalledPackages()
+{
+return array_keys(self::$installed['versions']);
+}
+
+
+
+
+
+
+
+
+
+public static function isInstalled($packageName)
+{
+return isset(self::$installed['versions'][$packageName]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public static function satisfies(VersionParser $parser, $packageName, $constraint)
+{
+$constraint = $parser->parseConstraints($constraint);
+$provided = $parser->parseConstraints(self::getVersionRanges($packageName));
+
+return $provided->matches($constraint);
+}
+
+
+
+
+
+
+
+
+
+
+public static function getVersionRanges($packageName)
+{
+if (!isset(self::$installed['versions'][$packageName])) {
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+$ranges = array();
+if (isset(self::$installed['versions'][$packageName]['pretty_version'])) {
+$ranges[] = self::$installed['versions'][$packageName]['pretty_version'];
+}
+if (array_key_exists('aliases', self::$installed['versions'][$packageName])) {
+$ranges = array_merge($ranges, self::$installed['versions'][$packageName]['aliases']);
+}
+if (array_key_exists('replaced', self::$installed['versions'][$packageName])) {
+$ranges = array_merge($ranges, self::$installed['versions'][$packageName]['replaced']);
+}
+if (array_key_exists('provided', self::$installed['versions'][$packageName])) {
+$ranges = array_merge($ranges, self::$installed['versions'][$packageName]['provided']);
+}
+
+return implode(' || ', $ranges);
+}
+
+
+
+
+
+public static function getVersion($packageName)
+{
+if (!isset(self::$installed['versions'][$packageName])) {
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+if (!isset(self::$installed['versions'][$packageName]['version'])) {
+return null;
+}
+
+return self::$installed['versions'][$packageName]['version'];
+}
+
+
+
+
+
+public static function getPrettyVersion($packageName)
+{
+if (!isset(self::$installed['versions'][$packageName])) {
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+if (!isset(self::$installed['versions'][$packageName]['pretty_version'])) {
+return null;
+}
+
+return self::$installed['versions'][$packageName]['pretty_version'];
+}
+
+
+
+
+
+public static function getReference($packageName)
+{
+if (!isset(self::$installed['versions'][$packageName])) {
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+if (!isset(self::$installed['versions'][$packageName]['reference'])) {
+return null;
+}
+
+return self::$installed['versions'][$packageName]['reference'];
+}
+
+
+
+
+
+public static function getRootPackage()
+{
+return self::$installed['root'];
+}
+
+
+
+
+
+
+
+public static function getRawData()
+{
+return self::$installed;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public static function reload($data)
+{
+self::$installed = $data;
+}
+}
--- wp-content/plugins/get_commit.php
+++ wp-content/plugins/get_commit.php
@@ -1,49 +1,84 @@
 <?php
-//get_commit.php
+//widget
 
 /*
-Plugin Name: Plugin Widget
-Plugin URI: http://wordpress.org/plugins/widget/
-Description: affichage du "Niveau de la mer à Saint Jean de Luz" pour le site https://www.tek2020lucile/
+Plugin Name: Plugin get_commit
+Plugin URI: http://wordpress.org/plugins/get_commit/
+Description: affichage pour le site https://www.wordpress/
 Version: 1.0
 Author: Lucile Christmann
 */
 require_once __DIR__ . '/vendor/autoload.php';
 
+class get_commit extends WP_Widget 
+{
 
-# paramétrages
-$sCompteGithub = "CHRISTMANNlucile";
-$sRepository = "github_commit";
-$nNombreCommit = 10;
-
-# Faire la requete à l'API
-$client = new \Github\Client();
-try {
-    $aCommits = $client->api('repo')->commits()->all($sCompteGithub, $sRepository, ['path' => ""]);
-
-    // Affichage des resultats
-    $nCnt = 0;
-    while ( ($nCnt < $nNombreCommit) && isset($aCommits[$nCnt]) ) {
-
-      viewCommit( $aCommits[$nCnt] );
-
-      $nCnt++;
-    }
-
-
-    foreach ($aCommits as $key => $aCommit) {
-
-    }
-
-  } catch (\RuntimeException $e) {
-    echo "Erreur acces API Github";
+  public function __construct()
+  {
+      parent::__construct('get_commit', 'get_commit', array( 'customize_selective_refresh' => true,));
+     
   }
 
-function viewCommit( $aCommit )
-{
-  echo str_repeat('-', 50) . PHP_EOL;
-  echo "Message : " . $aCommit['commit']['message'] . PHP_EOL;
-  echo "Author  : " . $aCommit['commit']['author']['name'] . PHP_EOL;
-  echo "Date    : " . $aCommit['commit']['author']['date'] . PHP_EOL;
-  echo PHP_EOL;
+  public function widget( $args, $instance ) 
+  {
+   # paramétrages
+  $sCompteGithub = "CHRISTMANNlucile";
+  $sRepository = "wordpress";
+  $nNombreCommit = 10;
+
+  # Faire la requete à l'API
+  $client = new \Github\Client();
+  try {
+      $aCommits = $client->api('repo')->commits()->all($sCompteGithub, $sRepository, ['path' => ""]);
+
+      // Affichage des resultats
+      $nCnt = 0;
+      while ( ($nCnt < $nNombreCommit) && isset($aCommits[$nCnt]) ) {
+
+        viewCommit( $aCommits[$nCnt] );
+
+        $nCnt++;
+      }
+
+
+      foreach ($aCommits as $key => $aCommit) {
+
+      }
+
+    } catch (\RuntimeException $e) {
+      echo "Erreur acces API Github";
+    }
+  }
+
+ public function form( $instance ) 
+  {
+    if ( isset( $instance[ 'title' ] ) )
+    $title = $instance[ 'title' ];
+    else
+    $title = __( 'Default Title', 'get_commit_widget_domain' );
+    ?>
+    <p>
+    <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+    <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+    </p>
+    <?php
+  }
+
+  public function update( $new_instance, $old_instance ) 
+  {
+    $instance = array();
+    $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+    return $instance;
+  }
+
+  function get_commit_register_widget() 
+  {
+    register_widget( 'get_commit_widget' );
+  }
+            
+
+            
+
 }
+add_action( 'widgets_init', 'get_commit_register_widget' );
+
