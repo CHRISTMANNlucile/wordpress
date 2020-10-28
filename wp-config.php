ad76a0ef9f542b74427de13da244394ba6b6cf19
luz <luz@macbook-air-de-luz.home>
2020-10-28 15:23:50 +0100

    mercredi2

--- wp-config.php
+++ wp-config.php
@@ -35,7 +35,7 @@ define( 'DB_HOST', 'localhost' );
 
 /** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
 define( 'DB_CHARSET', 'utf8' );
-
+//define( 'WP_DEBUG', true );
 /**
  * Type de collation de la base de données.
  * N’y touchez que si vous savez ce que vous faites.
--- wp-content/plugins/get_commit.php
+++ wp-content/plugins/get_commit.php
@@ -1,7 +1,13 @@
 <?php
 //widget
 
-
+/*
+Plugin Name: Plugin Commit
+Plugin URI: http://wordpress.org/plugins/get_commit/
+Description: affichage les 10 derniers commits d'un dépot Github 
+Version: 1.0
+Author: Lucile Christmann
+*/
 
 require_once __DIR__ . '/vendor/autoload.php';
 
@@ -14,34 +20,42 @@ class get_commit extends WP_Widget
      
   }
 
-  public function widget( $args, $instance ) 
+  function widget( $args,$instance )
   {
-   
+      extract( $args ); 
+      echo $before_widget;
+      echo $before_title . $instance["titre"] . $after_title;
+      include_once(ABSPATH . WPINC . '/rss.php');
+      wp_rss($instance["url"]);
+  }    
+
+    public function get_github_commits($user, $repo, $count)
+  {
    # paramétrages
-  $sCompteGithub = "anoop4real";
-  $sRepository = "alexa-disheroes";
-  $nNombreCommit = 10;
-  $response = wp_remote_get( 'https://github.com/KnpLabs/php-github-api' );
-  $body     = wp_remote_retrieve_body( $response );
-  $http_code = wp_remote_retrieve_response_code( $response );
-  $last_modified = wp_remote_retrieve_header( $response, 'last-modified' );
-  wp_remote_get( $url, $args );
-  $args = array(
-    'headers' => array(
-        'Authorization' => 'Basic ' . base64_encode( YOUR_USERNAME . ':' . YOUR_PASSWORD )
-    )
-);
+   $user = "CHRISTMANNlucile";
+   $repo = "github_commit";
+   $count = 10;
 
   # Faire la requete à l'API
- $client = new \Github\Client();
+  $client = new GuzzleHttp\Client();
+  $response = $client->get('http://guzzlephp.org');
+  $res = $client->get('https://github.com/KnpLabs/php-github-api', ['auth' =>  ['user', 'pass']]);
+  
   try {
-      $aCommits = $client->api('repo')->commits()->all($sCompteGithub, $sRepository, ['path' => ""]);
+      $aCommits = $client->api('https://github.com/KnpLabs/php-github-api')->commits()->all($user, $repo, ['path' => ""]);
 
       // Affichage des resultats
       $nCnt = 0;
-      while ( ($nCnt < $nNombreCommit) && isset($aCommits[$nCnt]) ) {
+      while ( ($nCnt < $count) && isset($aCommits[$nCnt]) ) {
 
-        viewCommit( $aCommits[$nCnt] );
+        echo $res->getStatusCode();
+        // "200"
+        echo $res->getHeader('content-type');
+        // 'application/json; charset=utf8'
+        echo $res->getBody();
+        // {"type":"User"...'
+        var_export($res->json());
+        // Outputs the JSON decoded data
 
         $nCnt++;
       }
@@ -58,23 +72,53 @@ class get_commit extends WP_Widget
 
 function form( $instance ) 
   {
-    if ( isset( $instance[ 'title' ] ) )
-    $title = $instance[ 'title' ];
-    else
-    $title = __( 'Default Title', 'get_commit_widget_domain' );
-    ?>
-    <p>
-    <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
-    <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
-    </p>
-    <?php
+        $defaut = array(
+            'titre'=> "widget d'exemple",
+        );
+        $instance = wp_parse_args( $instance, $defaut)
+        ?>
+        <p> 
+        <label for="<?php echo $this->get_field_id( 'title' ); ?>">'Title:' </label>
+        <input value="<?php echo $instance['title']; ?>" name="<?php echo $this->get_field_name("title"); ?>" id="<?php echo $this->get_field_id("titre");?>"type="text"/>
+        </p>
+       
+        <p> 
+        <label for="<?php echo $this->get_field_id( 'url' ); ?>">'url:' </label>
+        <input value="<?php echo $instance['url']; ?>" name="<?php echo $this->get_field_name("url"); ?>" id="<?php echo  $this->get_field_id("url"); ?>"type="text"/>
+        </p>
+
+        <p>
+            <label for="rss-url-3">saisissezl'url ici:</label>
+            <input id="rss-url-3" class="widefat" name="widget-rss[url]"
+            type="text value="">
+        </p>
+        <label for="rss-items-3">Combien d'entrées souhaitez-vous afficher?</label>
+        <p>
+        <select id="rss-items-3" name="widget-rss[3][items]
+            <option value="1">1</option>
+            <option value="1">2</option>
+            <option value="1">3</option>
+            <option value="1">4</option>
+            <option value="1">5</option>
+            <option value="1">6</option>
+            <option value="1">7</option>
+            <option value="1">8</option>
+        </select>
+        </p>
+        <p>
+        <input id="rss-show-summary-3" name="widget-rss[3][show-summary]"
+        type="checkbox" value="1">
+            <label for="rss-show-summary-3">Afficher le contenu de l'élément</label>
+            <br>
+            <input id="rss-show-author-3" name="widget-rss[3][show_author]"
+            type="checkbox" value="1">
+        </p>
+     <?php 
   }
 
 function update( $new_instance, $old_instance ) 
   {
-    $instance = array();
-    $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
-    return $instance;
+        return $new_instance;
   }
 
 function get_commit_register_widget() 
